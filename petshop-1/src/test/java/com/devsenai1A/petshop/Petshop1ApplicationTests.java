5cfb96288e08f2168ef44b1069097a99967ced1f
brecarrenho <brenda.carrenho1a@gmail.com>
2025-09-22 14:13:38 -0300

    Add files via upload

---
+++ petshop-1/src/test/java/com/devsenai1A/petshop/Petshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenai1A.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
